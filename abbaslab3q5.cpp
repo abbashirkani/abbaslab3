adfae9066d5f751a1a7db72ec4be33efea4616ec
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 00:15:51 +0530

    Create abbaslab3q5.cpp

---
+++ abbaslab3q5.cpp
@@ -0,0 +1,21 @@
+# abbaslab3
+// question 5
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"enter anumber\n";
+  cin>>a;
+  if(a%2==0)
+  {
+    cout<<a<<" is an even number"<<endl;
+  }
+  else
+  {
+    cout<<a<<" is an odd number"<<endl;
+  }
+  return 0;
+}
