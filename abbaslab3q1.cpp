af01aac7fd1a81076a1cc2db8ab0f9621168396b
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 00:07:06 +0530

    Create abbaslab3q1.cpp

---
+++ abbaslab3q1.cpp
@@ -0,0 +1,21 @@
+# abbaslab3
+// question1
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   cout<<"enter any two numbers \n";
+   cin>>a>>b;
+   if(a<b)
+   {
+     cout<<"the maximum number of the two numbers is"<<b<<endl;
+   }
+   else
+   {   
+     cout<<"the maximum number of the two numbers is"<<a<<endl;
+   }
+   return 0;
+}
