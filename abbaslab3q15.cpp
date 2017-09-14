3bec12af61db6b385a5634126186ec113dee4df7
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 00:38:08 +0530

    Create abbaslab3q15.cpp

---
+++ abbaslab3q15.cpp
@@ -0,0 +1,23 @@
+  # abbaslab3
+  
+ //question 15
+ 
+ 
+ 
+ #include<iostream>
+using namespace std;
+int main()
+{
+   float a,b,c,s;
+   cout<<"enter three sides";
+   cin>>a>>b>>c;
+   if((a+b>c)&&(b+c>a)&&(a+c>b))
+   {
+    cout<<"the triangle is valid for sides "<<"\n"<<a<<"\n"<<b<<"\n"<<c<<endl;
+   }
+   else
+   {
+    cout<<"the triangle is not valid"<<endl;
+   }
+   return 0;
+}
