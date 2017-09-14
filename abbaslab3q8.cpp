4a24ccc9afc3ced5c57cea56dbdd2eb760bbfe72
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 00:23:18 +0530

    Create abbaslab3q8.cpp

---
+++ abbaslab3q8.cpp
@@ -0,0 +1,21 @@
+# abbaslab3
+// question
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   char c;
+   cout<<"enter an alphabet\n";
+   cin>>c;
+   if((c=='a'||c=='A')||(c=='e'||c=='E')||(c=='i'||c=='I')||(c=='o'||c=='O')||(c      =='u'||c=='U'))
+   {
+     cout<<c<<" is a vowel"<<endl;
+   }
+   else
+   {
+     cout<<c<<" is a consonant"<<endl;
+   }
+   return 0;
+}
