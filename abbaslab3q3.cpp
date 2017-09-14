38c541b6bf0ae772dd458400bb1b123171a0df56
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 00:12:34 +0530

    Create abbaslab3q3.cpp

---
+++ abbaslab3q3.cpp
@@ -0,0 +1,25 @@
+# abbaslab3
+// question 3
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a;
+    cout<<"enter a number\n";
+    cin>>a;
+    if(a>0)
+    { 
+      cout<<a<<" the number is positive"<<endl;
+    }
+    else if(a<0)
+    {
+      cout<<a<<" the number is negative"<<endl;
+    }
+    else
+    {
+      cout<<a<<" is zero"<<endl;
+    }
+    return 0;
+}
