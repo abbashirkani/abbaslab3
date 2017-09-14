721e74765748f750fbebd2287c7605940fd8e0c5
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 00:10:09 +0530

    Create abbaslab3q2.cpp

---
+++ abbaslab3q2.cpp
@@ -0,0 +1,24 @@
+# abbaslab3
+// question 2
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"enter any three numbers \n";
+    cin>>a>>b>>c;
+    if(a>b&&a>c)
+    { 
+      cout<<"the maximum of three numbers is-"<<a<<endl;
+    }
+    else if(b>c&&b>a)
+    {
+      cout<<"the maximum of three numbers is-"<<b<<endl;
+    }
+    else
+    {
+      cout<<"the maximum of three numbers is-"<<c<<endl;
+    }
+    return 0;
+}
