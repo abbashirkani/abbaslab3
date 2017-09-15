e2386c68bddc2dab9f74a4c1de0e7f7604cfd008
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 21:48:03 +0530

    Create abbaslab3q22.cpp

---
+++ abbaslab3q22.cpp
@@ -0,0 +1,18 @@
+# abbaslab3
+// q 22
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, i=1;
+	cout<<"Enter n natural number\n";
+	cin>>n;
+	while(i<=n)
+	{
+		cout<<i<<"\n";
+		i++;
+	}
+	return 0;
+}
