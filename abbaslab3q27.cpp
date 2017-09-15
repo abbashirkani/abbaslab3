b40dc98edb2d1dfc3736ae421f583f20463412f8
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 21:56:44 +0530

    Create abbaslab3q27.cpp

---
+++ abbaslab3q27.cpp
@@ -0,0 +1,24 @@
+# abbaslab3
+// q 27
+
+
+include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int n,i,sum=0;
+	cout<<"enter n natural number\n";
+	cin>>n;
+	i=1;
+	while(i<=n)
+	{
+	   sum=sum+i;
+		
+	}
+	   
+	cout<<"Sum of all natural numbers upto n is "<<sum<<endl;
+	
+	return 0;
+}
