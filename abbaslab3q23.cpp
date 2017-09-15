ab137476edc5f5dd9de37c140d7daa6d22ac5857
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 21:49:17 +0530

    Create abbaslab3q23.cpp

---
+++ abbaslab3q23.cpp
@@ -0,0 +1,21 @@
+# abbaslab3
+// q 23
+
+
+include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int n,i;
+	cout<<"enter n natural number\n";
+	cin>>n;
+	i=n;
+	while(i>=1)
+	{
+		cout<<i<<"\n";
+		i--;
+	}
+	return 0;
+}
