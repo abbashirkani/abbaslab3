cc54b2e025b3b7e2f476f38b242f5871b407e65d
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 21:57:53 +0530

    Create abbaslab3q28.cpp

---
+++ abbaslab3q28.cpp
@@ -0,0 +1,22 @@
+# abbaslab3
+// q 28
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int n,s=0,i;
+	cout<<"enter n natural number\n";
+	cin>>n;
+	i=2;
+	while(i<=n)
+	{
+		s=s+i;
+		i=i+2;
+	}
+	cout<<"the sum of all even numbers is ="<<s;
+	return 0;
+}
