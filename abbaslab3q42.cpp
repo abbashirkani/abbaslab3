88d084dedd2cdfcfd9a5cbb01616991a705644a4
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 22:25:47 +0530

    Create abbaslab3q42.cpp

---
+++ abbaslab3q42.cpp
@@ -0,0 +1,21 @@
+# abbaslab3
+// q 42
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ int x,n;
+ cout<<"Enter a number : ";
+ cin>>n;
+ cout<<"Factors of "<<n<<"=";
+ for(x=1;x<=n;x++)
+ {
+   if(n%x==0)
+   {
+    cout<<x<<" ";
+   }
+  }
+ return 0;
+}
