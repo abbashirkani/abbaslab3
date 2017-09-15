70e83c5c52b257452e1894863abbcf45140ca3df
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 22:05:56 +0530

    Create abbaslab3q33.cpp

---
+++ abbaslab3q33.cpp
@@ -0,0 +1,22 @@
+# abbaslab3
+// q 33
+
+
+include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,l,f,s;
+	cout<<"enter a number "<<endl;
+	cin>>n;
+	l=n%10;
+	while(n>10)
+	{
+		f=n%10;
+           n=n/10;
+		
+	}
+	s=l+f;
+	cout<<"the sum of the first and last digit is "<<s<<endl;
+	return 0;
+}
