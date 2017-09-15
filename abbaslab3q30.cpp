5968fc47ac7ed0c01ac2e0afb13a12302db95f4a
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-15 22:01:31 +0530

    Create abbaslab3q30.cpp

---
+++ abbaslab3q30.cpp
@@ -0,0 +1,20 @@
+# abbaslab3
+// q 30
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,m;
+	cout<<"enter any number"<<endl;
+	cin>>n;
+	i=1;
+	while(i<=10)
+	{
+	  m=n*i;
+	  cout<<n<<'*'<<i<<'='<<m<<endl;
+	  i++;
+	}
+	return 0;
+}
